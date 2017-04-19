c02d681a02eb8e182a80d5657ed8f5a66d45c522
AsadShaik <asadshaik1103@gmail.com>
2017-04-19 14:54:26 +0530

    Commited

--- app/src/main/java/com/example/asadshaik/mjcettimetable/Profile_Student.java
+++ app/src/main/java/com/example/asadshaik/mjcettimetable/Profile_Student.java
@@ -11,7 +11,11 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +24,15 @@ public class Profile_Student extends AppCompatActivity implements AdapterView.On
 
     Spinner branch,year,section;
     Button fetch;
+
+    //firebase auth object
+    private FirebaseAuth firebaseAuth;
+
+    //view objects
+    private TextView textViewUserEmail;
+    private Button buttonLogout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +47,43 @@ public class Profile_Student extends AppCompatActivity implements AdapterView.On
         year.setOnItemSelectedListener(this);
         section.setOnItemSelectedListener(this);
 
+        //initializing firebase authentication object
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //if the user is not logged in
+        //that means current user will return null
+        if(firebaseAuth.getCurrentUser() == null){
+            //closing this activity
+            finish();
+            //starting login activity
+            startActivity(new Intent(getApplicationContext(), LoginActivityStudent.class));
+        }
+
+        //getting current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        //initializing views
+        //textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+
+        //displaying logged in user name
+        //textViewUserEmail.setText("Welcome "+user.getEmail());
+
+        //adding listener to button
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //if logout is pressed
+                if(v == buttonLogout){
+                    //logging out the user
+                    firebaseAuth.signOut();
+                    //closing activity
+                    finish();
+                    //starting login activity
+                    startActivity(new Intent(getApplicationContext(), LoginActivityStudent.class));
+                }
+            }
+        });
         //spinner 1
         List<String> categories_branch = new ArrayList<String>();
         categories_branch.add("IT");
--- app/src/main/java/com/example/asadshaik/mjcettimetable/Timetable_data.java
+++ app/src/main/java/com/example/asadshaik/mjcettimetable/Timetable_data.java
@@ -52,35 +52,35 @@ public class Timetable_data extends AppCompatActivity {
 
 
 
-        if (data1=="IT" && data2=="1" && data3=="A"){
+        if (data1.equals("IT") && data2.equals("1") && data3.equals("A")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fit%2Fit_1%3AA.jpeg?alt=media&token=1d2ed5ce-efba-44c2-9ecd-ebd37fe15bc8").into(timetable);
 
         }
-        else if (data1=="IT" && data2=="1" && data3=="B"){
+        else if (data1.equals("IT") && data2.equals("1") && data3.equals("B")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fit%2Fit_1%3AB.jpeg?alt=media&token=fd24b774-382e-46b0-8df1-a4c291167466").into(timetable);
 
         }
-        else if (data1=="IT" && data2=="2" && data3=="A"){
+        else if (data1.equals("IT") && data2.equals("2") && data3.equals("A")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fit%2Fit_2%3AA.jpeg?alt=media&token=a260d208-b8f9-4069-a083-ed4939f0f091").into(timetable);
 
         }
-        else if (data1=="IT" && data2=="2" && data3=="B"){
+        else if (data1.equals("IT") && data2.equals("2") && data3.equals("B")){
 
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fit%2Fit_2%3AB.jpeg?alt=media&token=4c0f65df-e521-4b24-b9ed-529ddb6d9ea9").into(timetable);
         }
-        else if (data1=="IT" && data2=="3" && data3=="A"){
+        else if (data1.equals("IT") && data2.equals("3") && data3.equals("A")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fit%2Fit_3%3AA.jpeg?alt=media&token=a2d533ff-ccc2-4068-b82a-aa48bb3c620c").into(timetable);
 
         }
-        else if (data1=="IT" && data2=="3" && data3=="B"){
+        else if (data1.equals("IT") && data2.equals("3") && data3.equals("B")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fit%2Fit_3%3AB.jpeg?alt=media&token=1dc582c2-3ec7-48cc-9636-2258704bd190").into(timetable);
 
         }
-        else if (data1=="IT" && data2=="4" && data3=="A"){
+        else if (data1.equals("IT") && data2.equals("4") && data3.equals("A")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fit%2Fit_4%3AA.jpeg?alt=media&token=d37ab9ba-5602-4640-bafd-23bb92c61924").into(timetable);
 
         }
-        else if (data1=="IT" && data2=="4" && data3=="A"){
+        else if (data1.equals("IT") && data2.equals("4") && data3.equals("B")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fit%2Fit_4%3AB.jpeg?alt=media&token=d28e3fd5-31c4-4d2b-80c4-4708eebb62c4").into(timetable);
 
         }
@@ -88,70 +88,75 @@ public class Timetable_data extends AppCompatActivity {
 
 
         //CSE
-        else if (data1=="CSE" && data2=="1" && data3=="A"){
+        else if (data1.equals("CSE") && data2.equals("1") && data3.equals("A")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fcse%2Fcse_1%3AA.jpeg?alt=media&token=4889f5c8-d020-4d6e-a0c3-c669fb339fa1").into(timetable);
 
 
         }
-        else if (data1=="CSE" && data2=="1" && data3=="B"){
+        else if (data1.equals("CSE") && data2.equals("1") && data3.equals("B")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fcse%2Fcse_1%3AB.jpeg?alt=media&token=00f4d961-d904-43cc-86bf-6148eb9df9ea").into(timetable);
 
         }
-        else if (data1=="CSE" && data2=="2" && data3=="A"){
+        else if (data1.equals("CSE") && data2.equals("2") && data3.equals("A")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fcse%2Fcse_2%3AA.jpeg?alt=media&token=f917eade-b702-4e96-b9ae-7da226d346d0").into(timetable);
 
         }
-        else if (data1=="CSE" && data2=="2" && data3=="B"){
+        else if (data1.equals("CSE") && data2.equals("2") && data3.equals("B")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fcse%2Fcse_2%3AB.jpeg?alt=media&token=e31151e8-0a06-4bdb-b895-662d612809e4").into(timetable);
 
         }
-        else if (data1=="CSE" && data2=="3" && data3=="A"){
+        else if (data1.equals("CSE") && data2.equals("3") && data3.equals("A")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fcse%2Fcse_3%3AA.jpeg?alt=media&token=d2488751-7dfb-4efc-926e-7f670ff0b123").into(timetable);
 
         }
-        else if (data1=="CSE" && data2=="3" && data3=="B"){
+        else if (data1.equals("CSE") && data2.equals("3") && data3.equals("B")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fcse%2Fcse_3%3AB.jpeg?alt=media&token=42541706-4b41-4a3d-9fab-043f0bbbcbd6").into(timetable);
 
         }
-        else if (data1=="CSE" && data2=="4" && data3=="A"){
+        else if (data1.equals("CSE") && data2.equals("4") && data3.equals("A")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fcse%2Fcse_4%3AA.jpeg?alt=media&token=000bc805-8555-485a-8f6c-246e82e12758").into(timetable);
 
         }
-        else if (data1=="CSE" && data2=="4" && data3=="B"){
+        else if (data1.equals("CSE") && data2.equals("4") && data3.equals("B")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fcse%2Fcse_4%3AB.jpeg?alt=media&token=6c94935a-308a-47d2-9a75-6364ad527a57").into(timetable);
 
         }
+
+
+
+
+
         //CIVIL
-        else if (data1=="CIVIL" && data2=="1" && data3=="A"){
+        else if (data1.equals("CIVIL") && data2.equals("1") && data3.equals("A")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fcivil%2Fcivil_1%3AA.PNG?alt=media&token=f18a549c-cf35-4038-acde-5689beed69df").into(timetable);
 
 
         }
-        else if (data1=="CIVIL" && data2=="1" && data3=="B"){
+        else if (data1.equals("CIVIL") && data2.equals("1") && data3.equals("B")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fcivil%2Fcivil_1%3AB.PNG?alt=media&token=a5974fa9-afc0-42b0-8c3a-11ba549f2dd3").into(timetable);
 
         }
-        else if (data1=="CIVIL" && data2=="2" && data3=="A"){
+        else if (data1.equals("CIVIL") && data2.equals("2") && data3.equals("A")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fcivil%2Fcivil_2%3AA.PNG?alt=media&token=10797c19-9a2d-4c07-bb39-b3e9e3aac503").into(timetable);
 
         }
-        else if (data1=="CIVIL" && data2=="2" && data3=="B"){
+        else if (data1.equals("CIVIL") && data2.equals("2") && data3.equals("B")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fcivil%2Fcivil_2%3AB.PNG?alt=media&token=3e115803-4a99-449e-bc3d-a6c0b628981d").into(timetable);
 
         }
-        else if (data1=="CIVIL" && data2=="3" && data3=="A"){
+        else if (data1.equals("CIVIL") && data2.equals("3") && data3.equals("A")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fcivil%2Fcivil_3%3AA.PNG?alt=media&token=f6861002-2c19-4039-8cb2-3480300b5e46").into(timetable);
 
         }
-        else if (data1=="CIVIL" && data2=="3" && data3=="B"){
+        else if (data1.equals("CIVIL") && data2.equals("3") && data3.equals("B")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fcivil%2Fcivil_3%3AB.PNG?alt=media&token=a710b642-9ee5-4c3e-9712-8c938ecb9df3").into(timetable);
 
         }
-        else if (data1=="CIVIL" && data2=="4" && data3=="A"){
+        else if (data1.equals("CIVIL") && data2.equals("4") && data3.equals("A")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fcivil%2Fcivil_4%3AA.PNG?alt=media&token=e86fc344-6312-406e-aab9-ab4f37a0de15").into(timetable);
 
         }
-        else if (data1=="CIVIL" && data2=="4" && data3=="B"){
+        else if (data1.equals("CIVIL") && data2.equals("4") && data3.equals("B")){
             Picasso.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/mjcet-timetable.appspot.com/o/timetables%2Fcivil%2Fcivil_4%3AB.PNG?alt=media&token=b68d2d5b-5ba3-4b37-8f79-69142033b332").into(timetable);
 
         }
